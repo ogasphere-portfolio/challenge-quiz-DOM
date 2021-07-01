87f46821c8d84e0aa944ca795a04c29176661c5d
Christophe Deneuve <33314621+christopheOclock@users.noreply.github.com>
2021-07-01 16:01:38 +0200

    Initial commit

---
+++ js/questions.js
@@ -0,0 +1,11 @@
+let questions = [
+    'Combien de joueurs jouent sur un terrain de football (en Ligue 1 par exemple) ?',
+    'Combien font 6 x 12 ?',
+    'L\'eau est composée de combien d\'atomes ?'
+];
+
+let responses = [
+    22,
+    72,
+    3
+];
